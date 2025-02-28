bbd1fcaee5c9bc79dea97e4e8b6332a9b33ee8cf
Sajjad <shahbazi.developer@gmail.com>
2025-02-28 14:05:34 +0330

    add viewmodel

---
+++ app/src/main/java/com/example/tabdealinterviewproject/AssetsViewModel.kt
@@ -0,0 +1,47 @@
+package com.example.tabdealinterviewproject
+
+import androidx.lifecycle.ViewModel
+import com.example.common.model.Result
+import androidx.lifecycle.viewModelScope
+import com.example.domain.model.TokenRepoModel
+import com.example.domain.usecase.GetTokensUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class AssetsViewModel @Inject constructor(private val getAssetsUseCase: GetTokensUseCase) :
+    ViewModel() {
+
+    val delayTime : Long = 3000
+    val tokens = MutableSharedFlow<List<TokenRepoModel>>(replay = 0)
+    val isLoading = MutableSharedFlow<Boolean>(replay = 0)
+    val error = MutableSharedFlow<String?>(replay = 0)
+
+    suspend fun startAssetLoadingLoop() {
+        while (true) {
+            loadAssets()
+            delay(delayTime)
+        }
+    }
+
+    fun loadAssets() {
+        viewModelScope.launch {
+            isLoading.emit(true)
+            when (val result = getAssetsUseCase()) {
+                is Result.Success -> {
+                    tokens.emit(result.data.toList())
+                    error.emit(null)
+                }
+
+                is Result.Error -> {
+                    error.emit(result.exception.message ?: "An unexpected error occurred")
+                }
+            }
+            isLoading.emit(false)
+        }
+    }
+}
