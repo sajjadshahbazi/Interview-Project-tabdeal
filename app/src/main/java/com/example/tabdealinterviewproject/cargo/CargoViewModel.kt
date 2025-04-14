1c0750ae586eac5bf95b364c4e73efe50afbd8e7
Sajjad <shahbazi.developer@gmail.com>
2025-04-14 17:24:55 +0330

    add view models

---
+++ app/src/main/java/com/example/tabdealinterviewproject/cargo/CargoViewModel.kt
@@ -0,0 +1,71 @@
+package com.example.tabdealinterviewproject.cargo
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.common.model.Result
+import com.example.domain.model.CargoRepoModel
+import com.example.domain.usecase.GetCargosUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class CargoViewModel @Inject constructor(private val getCargosUseCase: GetCargosUseCase) : ViewModel() {
+    private val _cargoItems : MutableList<CargoRepoModel> = mutableListOf()
+    private var _selectedItem : CargoRepoModel? = null
+
+    val cargoItems = MutableSharedFlow<List<CargoRepoModel>>()
+    val selectedItem = MutableSharedFlow<CargoRepoModel?>()
+    val _isLoading = MutableSharedFlow<Boolean>()
+    val _error = MutableSharedFlow<String?>()
+    var lockedItems :Boolean = false
+
+    init {
+        fetchCargoItems()
+    }
+
+    private fun fetchCargoItems() {
+        viewModelScope.launch {
+            _isLoading.emit(true)
+            when (val result = getCargosUseCase()) {
+                is Result.Success -> {
+                    _cargoItems.clear()
+                    _cargoItems.addAll(result.data.toList())
+                    cargoItems.emit(_cargoItems)
+                }
+                is Result.Error -> {
+                    _error.emit(result.exception.message ?: "An unexpected error occurred")
+                }
+            }
+            _error.emit(null)
+            _isLoading.emit(false)
+        }
+    }
+
+    fun selectCargo(cargoRepoModel : CargoRepoModel) {
+        if (!lockedItems) {
+            lockedItems = true
+            _selectedItem = cargoRepoModel
+            _cargoItems.forEach { it.isSelected = ((_selectedItem?.id ?: -1) == (it.id)) }
+
+            viewModelScope.launch {
+                _selectedItem?.let {
+                    selectedItem.emit(it)
+                }
+                cargoItems.emit(_cargoItems)
+            }
+        }
+    }
+
+    fun clearSelectedCargo() {
+        lockedItems = false
+        _selectedItem = null
+        _cargoItems.forEach { it.isSelected = false }
+        viewModelScope.launch {
+            selectedItem.emit(_selectedItem)
+            cargoItems.emit(_cargoItems)
+        }
+    }
+}
